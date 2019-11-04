0c23f13a96a7838fd94143aeb45346fe899c91d5
papabyte <bcreze@protonmail.com>
2019-11-04 10:46:35 +0100

    env variable + some doc

--- client/src/conf.js
+++ client/src/conf.js
@@ -7,5 +7,5 @@ exports.challenge_period_in_days = testnet ? 1/24 : 3;
 exports.challenge_min_stake_gb = 0.001;
 exports.challenge_coeff = 1.5;
 exports.gb_to_bytes = 1000000000;
-exports.website_name = "Counterstake.com";
+exports.website_name = "Counterstats.org";
 
